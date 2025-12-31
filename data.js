33193c5588399aaa41297329535204c512895426
aramkurdish <arambarsa@gmail.com>
2026-01-01 02:45:52 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const menuItems = [
+    { id: 1, name: "پیزای گۆشت", price: 5000, category: "pizza", img: "https://i.postimg.cc/k5rZvjrz/20251231-232813-0000.jpg", ingredients: "گۆشت، پەنیر، قارچک" },
+    { id: 2, name: "پیتزا مریشک", price: 5000, category: "pizza", img: "https://i.postimg.cc/Vv7p9m1R/20251231-232937-0000.jpg", ingredients: "مریشک، پەنیر، سۆس" },
+    { id: 3, name: "بووم بەرگر", price: 6000, category: "burger", img: "https://images.unsplash.com/photo-1594212699903-ec8a3eca50f5?w=200", ingredients: "گۆشت، پەنیر، کاهوو" },
+    { id: 4, name: "شاوەرمەی ساج", price: 3000, category: "shawarma", img: "https://i.postimg.cc/Pr91C9Z7/arabic-chicken-shawarma-sandwich-recipe-1747792750.jpg", ingredients: "مریشک، پەتاتە، سیر" }
+];
+
+const toppingsList = [
+    { id: 'cheese', name: 'پەنیری زیادە', price: 1000 },
+    { id: 'mushroom', name: 'قارچکی زیادە', price: 500 },
+    { id: 'sauce', name: 'سۆسی زیادە', price: 250 }
+];
